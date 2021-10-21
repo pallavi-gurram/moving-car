82d74a6d567613ac68d0542cc714ddd1bf6b9b01
Pallavi Gurram <78718783+pallavi-gurram@users.noreply.github.com>
2021-10-21 11:12:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $surface= $('.surface');
+    $car=$('.car');
+    $img=$('.car img')
+    let flag = true;
+   const cars = ['/assets/car1.png' , '/assets/car2.png']
+    $(document).on('keypress',function(e){
+        if(e.which==13){
+            $($surface).toggleClass('moveRight');
+            $($car).toggleClass('suspension');
+        }
+    })
+
+        $(document).on('keypress',function(e){
+            if(e.which==119){
+                if(flag){
+                    flag=false;
+                    $img.attr('src',cars[0]);
+                }
+                else{
+                    flag=true;
+                    $img.attr('src',cars[1]);
+                }
+            }
+
+    })
+});
